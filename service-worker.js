a86b15bd41dfa9f4640827f5721e7c65109f4f50
fcgod15 <noguerad742@gmail.com>
2025-06-29 16:18:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,15 @@
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
 function getYmid() {
-  try {
-    return new URL(self.location.href).searchParams.get('ymid');
-  } catch (e) {
-    console.warn(e);
-  }
+  try { return new URL(self.location.href).searchParams.get('ymid'); }
+  catch (e) { console.warn(e); }
   return null;
 }
 
 function getVar() {
-  try {
-    return new URL(self.location.href).searchParams.get('var');
-  } catch (e) {
-    console.warn(e);
-  }
+  try { return new URL(self.location.href).searchParams.get('var'); }
+  catch (e) { console.warn(e); }
   return null;
 }
 
